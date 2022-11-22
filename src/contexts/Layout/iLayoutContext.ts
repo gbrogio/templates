ff76276e7294e8186e1cb288aeff3c85243f5637
GBrogio <guilhermebrogio.ps@gmail.com>
2022-11-22 13:33:43 -0300

    chore: remove Layout context

--- src/contexts/Layout/iLayoutContext.ts
+++
@@ -1,6 +0,0 @@
-import type { iTheme } from 'types/iTheme';
-
-export interface iLayoutContext {
-  currTheme: iTheme;
-  currWidth: number;
-}
--- src/contexts/Layout/useLayoutContext.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { LayoutContext } from '.';
-
-export const useLayoutContext = () => useContext(LayoutContext);
